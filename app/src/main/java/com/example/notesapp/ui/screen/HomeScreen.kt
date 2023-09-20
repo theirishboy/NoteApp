3595371b2b7993d24b01da25bf9ace00aa1f8ccd
Pierre GABRIEL <pgab@mac-mini-de-pierre.home>
2023-09-20 08:49:12 +0200

    maj for work somewhere else

--- app/src/main/java/com/example/notesapp/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/notesapp/ui/screen/HomeScreen.kt
@@ -1,19 +1,56 @@
 package com.example.notesapp.ui.screen
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.notesapp.ui.theme.NotesAppTheme
 
 class HomeScreen {
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenBody(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Scaffold(topBar = {},
+    floatingActionButton = {
+        FloatingActionButton(onClick = { }) {
+        Icon(Icons.Default.Add, contentDescription = "add new note" )
+    }
+    }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    You have pressed the floating action button  times.
+                """.trimIndent(),
+            )
+        }
+
+        
+    }
 }
 
 @Preview(showBackground = true)
