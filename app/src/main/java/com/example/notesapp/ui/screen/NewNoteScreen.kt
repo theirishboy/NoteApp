cf5f96dedb92a885746e646817ce305108233840
Pierre GABRIEL <pgab@mac-mini-de-pierre.home>
2023-09-25 09:56:28 +0200

    End save new note front

--- app/src/main/java/com/example/notesapp/ui/screen/NewNoteScreen.kt
+++ app/src/main/java/com/example/notesapp/ui/screen/NewNoteScreen.kt
@@ -1,37 +1,36 @@
 package com.example.notesapp.ui.screen
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.notesapp.R
 import com.example.notesapp.ui.AppViewModelProvider
@@ -44,15 +43,17 @@ object NewNoteDestination : NavigationDestination {
 @Composable
 fun NewNoteScreen(navigateToNewNote: () -> Unit,
                   navigateBack: () -> Unit,
-                  viewModel: HomeScreenViewModel = viewModel(factory = AppViewModelProvider.Factory)
+                  viewModel: NewNoteViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
-    NewNoteBody()
+    val newNoteUiState = viewModel._newNoteUiState.collectAsState()
+
+    NewNoteBody(newNoteUiState.value)
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewNoteBody() {
+fun NewNoteBody(newNoteUiState: NewNoteUiState) {
    Scaffold (topBar = { NewNoteTopBar() },
        bottomBar = {
            BottomAppBar(
@@ -73,22 +74,39 @@ fun NewNoteBody() {
                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
                    ) {
-                       Icon(Icons.Filled.Edit, "Localized description")
+                       Icon(painter = painterResource(id = R.drawable.baseline_save_24), "Save Button")
                    }
                }
            )
        }){
        innerpadding->
            Column (modifier = Modifier.padding(innerpadding)){
-               Text(
-                   text = "Titre beuwqhfwsiaughfioasduhasidfuyagdfiu agsukfafjuasdskfgayfgsadkufaydgfakdfyzsgk",
-                   fontWeight = FontWeight.Bold,
-                   fontSize = 20.sp,
-                   modifier = Modifier.padding(5.dp),
+              TextField(
+                   value = "Test",
+                   onValueChange = {},
+                   modifier = Modifier.fillMaxWidth(),
+                   label = { Text(text = "Title")},
+                   keyboardOptions = KeyboardOptions.Default.copy(
+                       imeAction = ImeAction.Done
+                   ),
+                   keyboardActions = KeyboardActions(
+                       onDone = {}
+                   ),
+                  shape = RectangleShape
                )
 
-               Divider(color = Color.Gray, thickness = 1.dp)
-               Text(text = "Content")
+               TextField(
+                   value = "Content",
+                   onValueChange = {},
+                   modifier = Modifier.fillMaxSize(),
+                   label = { Text(text = "Title")},
+                   keyboardOptions = KeyboardOptions.Default.copy(
+                       imeAction = ImeAction.Done
+                   ),
+                   keyboardActions = KeyboardActions(
+                       onDone = {}
+                   )
+               )
            }
 
    }
@@ -100,6 +118,6 @@ fun NewNoteBody() {
 @Preview
 @Composable
 fun NewNoteBodyPreview() {
-    NewNoteBody()
+    NewNoteBody(NewNoteUiState())
 
 }
--- app/src/main/java/com/example/notesapp/ui/screen/NewNoteViewModel.kt
+++ app/src/main/java/com/example/notesapp/ui/screen/NewNoteViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.notesapp.ui.screen
 
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.notesapp.data.Note
 import com.example.notesapp.data.NoteRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 data class NewNoteUiState(val title:String = "", val content : String = "")
 class NewNoteViewModel( noteRepository: NoteRepository) : ViewModel() {
-
+    val _newNoteUiState = MutableStateFlow(NewNoteUiState())
+    val uiState: StateFlow<NewNoteUiState> = _newNoteUiState.asStateFlow()
 
 }
