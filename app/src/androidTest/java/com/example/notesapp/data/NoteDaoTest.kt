0c5bf16eb24dcd80e84c4b25ec6b9c562641239c
Pierre GABRIEL <pgab@mac-mini-de-pierre.home>
2023-09-25 08:21:05 +0200

    fix unitTest and creation of insertTests

--- app/src/androidTest/java/com/example/notesapp/data/NoteDaoTest.kt
+++ app/src/androidTest/java/com/example/notesapp/data/NoteDaoTest.kt
@@ -5,16 +5,20 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.Flow
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.not
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.hamcrest.CoreMatchers.hasItem
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
+import java.util.concurrent.CountDownLatch
 
 @RunWith(AndroidJUnit4::class)
 class NoteDaoTest {
@@ -46,7 +50,25 @@ class NoteDaoTest {
     }
     @Test
     @Throws(Exception::class)
-    fun testSelectAllNote() {
+    fun testInsertNoteReturnTrue() = runBlocking  {
+        val note = Note(2,"note demo","Ceci est une note de demo")
+        noteDao.insertNote(note)
+        val latch = CountDownLatch(1)
+        val job = async(Dispatchers.IO) {
+            noteDao.getAllNotes().collect {
+                assertThat(it, hasItem(note))
+                latch.countDown()
+
+            }
+        }
+        withContext(Dispatchers.IO) {
+            latch.await()
+        }
+        job.cancelAndJoin()
+    }
+    @Test
+    @Throws(Exception::class)
+    fun testSelectAllNote() = runBlocking {
         noteDao.deleteAll()
         val allNoteToTest : List<Note> = listOf(
             Note(1,"note demo autre","Ceci est une  autre note de demo"),
@@ -54,13 +76,19 @@ class NoteDaoTest {
             Note(3,"note demo autre encore","Ceci est encore une  autre note de demo"))
 
         for (element in allNoteToTest) noteDao.insertNote(element)
-        val allNoteRetrieve : Flow<List<Note>> = noteDao.getAllNotes()
+        val job = async(Dispatchers.IO) {
+            noteDao.getAllNotes().collect {
+                assertThat(it, hasItem(allNoteToTest))
 
-        assertEquals(allNoteToTest, allNoteRetrieve)
+            }
+
+        }
+        job.cancelAndJoin()
+
     }
     @Test
     @Throws(Exception::class)
-    fun testDeleteNoteById() {
+    fun testDeleteNoteById() = runBlocking {
         noteDao.deleteAll()
         val allNoteToTest : List<Note> = listOf(
             Note(1,"note demo autre","Ceci est une  autre note de demo"),
@@ -73,18 +101,14 @@ class NoteDaoTest {
         for (element in allNoteToTest) noteDao.insertNote(element)
 
         noteDao.deleteById(1)
+        val job = async(Dispatchers.IO) {
+            noteDao.getAllNotes().collect {
+                assertThat(it, hasItem(resultExceptedWithDeleteElement))
 
+            }
 
-        val allNoteRetrieve : List<Note>  = noteDao.getAllNotes()
+        }
+        job.cancelAndJoin()
 
-        assertEquals(resultExceptedWithDeleteElement, allNoteRetrieve)
     }
-//    @Test
-//    @Throws(Exception::class)
-//    fun createNote() {
-//        val note: Note = Note(1,"note demo","Ceci est une note de demo")
-//        noteDao.insert(note)
-//        val byName = noteDao.findnotesByName("george")
-//        assertThat(byName.get(0), equalTo(note))
-//    }
 }
--- app/src/main/java/com/example/notesapp/ui/screen/NewNoteViewModel.kt
+++ app/src/main/java/com/example/notesapp/ui/screen/NewNoteViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.notesapp.ui.screen
 
-class NewNoteViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.notesapp.data.Note
+import com.example.notesapp.data.NoteRepository
+
+data class NewNoteUiState(val title:String = "", val content : String = "")
+class NewNoteViewModel( noteRepository: NoteRepository) : ViewModel() {
+
+
 }
